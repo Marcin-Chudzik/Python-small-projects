b7da65968ad71af094f0c2a1684aa9ac92344d4a
Marcin_Chudzik <marcinchudzik01@gmail.com>
2022-08-19 00:00:49 +0200

    Using pandas to load .csv and .ods files, working on data

---
+++ data_visualization/pandas_exercises/prog_02.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+source_data = "files/dane_opady_temperatura.ods"
+print(f"Source data: {source_data}")
+
+df = pd.read_excel(source_data, engine="odf")
+
+print("----[ Information about source data]----")
+print(df)
+print("-----------------------------------------")
+
+# Adding a column
+df["Autor"] = "Marcin Chudzik"
+print("----[ Information about data]----")
+print(df)
+print("-----------------------------------------")
+
+# Libre Office Calc
+df.to_excel("files/dane_opady_temperatura_nowy.ods", engine="odf")
+# MS-Excel
+df.to_excel("files/dane_opady_temperatura_nowy.xlsx", engine="openpyxl")
